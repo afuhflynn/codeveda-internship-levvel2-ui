210f7e5ae261f7e1dda7703e00d52e5b039a2052
afuhflynn <flyinnsafuh@gmail.com>
2025-09-07 08:10:43 +0100

    feat: update server url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export const serverURL = "http://localhost:5000";
+export const serverURL =
+  "https://simpleapi-2ij6orshx-afuh-flynns-tembengs-projects.vercel.app/";
 export const LS_THEME = "fm_theme_v1";
 
 export const FILTERS = {
