6b3cba33ffaa477ed57953ba452e807eaac50ac6
afuhflynn <flyinnsafuh@gmail.com>
2025-09-08 12:33:06 +0100

    feat: update server url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
-export const serverURL =
-  "https://simpleapi-2ij6orshx-afuh-flynns-tembengs-projects.vercel.app/";
+export const serverURL = "https://simpleapi-two.vercel.app/";
 export const LS_THEME = "fm_theme_v1";
 
 export const FILTERS = {
